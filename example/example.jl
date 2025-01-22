d84eb31220618262e6ddd587bebd81b876211b49
Ezequiel Lozano <elozano@df.uba.ar>
2025-01-22 15:38:51 -0300

    Update Manifest.toml and refactor example.jl for improved package management

--- example/example.jl
+++ example/example.jl
@@ -3,48 +3,66 @@
 # It can be run as is, and it shouldn't throw any errors
 ####################################################################################################
 
-push!(LOAD_PATH, "./src/")
+cd(@__DIR__)
+
+using Pkg
+
+Pkg.activate("../")
+Pkg.instantiate()
+
+using Accessors, BibInternal, Bibliography, DataStructures, Glob, Unicode
+
+push!(LOAD_PATH, "../src/")
 using BibliographyFormatter
 
-####################################################################################################
-# Configuration variables
-####################################################################################################
+function bibliographyFormatter(
+    source_path::String,
+    output_path::String,
+    fields::Vector{String},
+)::Nothing
 
-# Which fields will appear in the end result. The order indicated here will be respected
-# If some non-essential fields don't exist in the source files, they will be ignored
-fields = [
-    "title",
-    "booktitle",
-    "author",
-    "publisher",
-    "institution",
-    "address",
-    "place",
-    "collection",
-    "series",
-    "edition",
-    "journal",
-    "volume",
-    "number",
-    "pages",
-    "year",
-    "month",
-    "url",
-    "isbn",
-]
+    final_bib = bib_formatter(source_path, fields)
 
-# Path to the source files
-source_path = joinpath(@__DIR__, "example_bib_files")
+    open(output_path, "w") do file
+        write(file, final_bib)
+    end;
 
-# Path to the output file (if it already exists it will be overwritten)
-output_path = joinpath(@__DIR__, "output.bibtex")
+    return nothing
 
-####################################################################################################
-# Formatting and saving the result in a file
-####################################################################################################
+end
 
-final_bib = bib_formatter(source_path, fields)
+function (@main)(ARGS)
 
-open(output_path, "w") do file
-    write(file, final_bib)
-end;
+    # Path to the source files
+    SOURCE_PATH = joinpath(@__DIR__, "example_bib_files")
+
+    # Path to the output file (if it already exists it will be overwritten)
+    OUTPUT_PATH = joinpath(@__DIR__, "output.bibtex")
+
+    # Which fields will appear in the end result
+    # The order will be respected
+    # If some non-essential fields don't exist in the source files, they will be ignored
+    FIELDS = [
+        "title",
+        "booktitle",
+        "author",
+        "publisher",
+        "institution",
+        "address",
+        "place",
+        "collection",
+        "series",
+        "edition",
+        "journal",
+        "volume",
+        "number",
+        "pages",
+        "year",
+        "month",
+        "url",
+        "isbn",
+    ]
+
+    bibliographyFormatter(SOURCE_PATH, OUTPUT_PATH, FIELDS)
+
+end
